c71498d67d0d77c01b44245d290e2272ed15285d
Mohan-this-side <50510359+Mohan-this-side@users.noreply.github.com>
2024-06-10 13:30:33 -0700

    Update streamlit_app.py
    new way of importing FAISS

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,12 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
 #from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 import os
-
-# Attempt to import FAISS and handle the error if not found
-try:
-    from langchain.vectorstores import FAISS
-except ModuleNotFoundError:
-    st.error("FAISS module not found. Please ensure you have installed 'faiss-cpu' and 'langchain' packages.")
-    st.stop()
 
 st.set_page_config(page_title="PaperPal", layout="wide")
 
